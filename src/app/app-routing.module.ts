fe92c8aa6682abc48ef22cf489678ea91066485b
Lizvan27 <lizvanmandy@gmail.com>
2023-01-13 10:25:47 -0500

    agregando ruta detalle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetallesComponent } from './pages/detalles/detalles.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path: 'detalles', component:DetallesComponent},
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { RouterLink } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DetallesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   exports:[
     HomeComponent
