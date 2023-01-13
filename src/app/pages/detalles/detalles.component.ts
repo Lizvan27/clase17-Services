707a2f7a1f981e0a84618881d22c6c82d7b6efdd
Lizvan27 <lizvanmandy@gmail.com>
2023-01-13 10:49:42 -0500

    tabla detalles

--- src/app/pages/detalles/detalles.component.ts
+++ src/app/pages/detalles/detalles.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/servicios/data.service';
 
 @Component({
   selector: 'app-detalles',
   templateUrl: './detalles.component.html',
 })
-export class DetallesComponent {
+export class DetallesComponent implements OnInit {
 
+  detalle: any= [];
+  constructor(private data:DataService){}
+
+  ngOnInit(): void {
+    this.data.getAll().subscribe(data => this.detalle = data)
+  }
 }
